928805a57327abf2d11cd0161c5b21f46dac430f
melonyme <zbogdangord@gmail.com>
2025-05-22 17:49:24 +0300

    first done

---
+++ src/1/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+
+	// x, y, o := parsing()
+	// res := calculating(x, y, o)
+
+	// res := calculating(parsing())
+
+	x := getOperand("Input left operand")
+	o := getOperation()
+	y := getOperand("Input right operand")	
+
+	res, err := calculating(x, y, o)
+
+	// if err == 3 {
+	// 	fmt.Println("divison by zero")
+	// } else {
+	// 	fmt.Printf("Result = %.3f\n", res)
+	// }
+
+	switch err {
+	case 3:
+		fmt.Println("division by zero")
+	case 0:
+		fmt.Printf("Result = %.3f\n", res)
+	}
+
+
+}
+
+func getOperand(s string) float64 {
+	for {
+		var input string
+		fmt.Println(s)
+		_, err := fmt.Scan(&input)
+		if err != nil {
+			fmt.Println("Invalid input")
+			continue
+		}
+
+		value, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+		if err != nil {
+			fmt.Println("Invalid input")
+			continue
+		}
+
+		return value
+	}
+}
+
+func getOperation() string {
+	for {
+		var input string
+		fmt.Print("Enter operation (+, -, *, /): ")
+		_, err := fmt.Scan(&input)
+		if err != nil {
+			fmt.Println("Invalid input")
+			continue
+		}
+
+		input = strings.TrimSpace(input)
+		if input == "+" || input == "-" || input == "*" || input == "/" {
+			return input
+		}
+
+		fmt.Println("Invalid input")
+	}
+}
+
+
+func calculating(x, y float64, o string) (float64, int) {
+	var res float64
+	err := 0
+	switch o {
+	case "+" :
+		res = x + y
+	case "-" :
+		res = x - y
+	case "/" :
+		if y == 0 {
+			err = 3
+		}
+		res = x / y
+	case "*" :
+		res = x * y
+	}	
+	
+	return res, err
+}
